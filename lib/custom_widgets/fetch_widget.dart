7ee5938f05252fe432c9bc017cec5719a6bfa787
danbourdier <dfbourdier@gmail.com>
2021-04-19 12:12:32 -0400

    constructed fetch method for album

--- lib/custom_widgets/fetch_widget.dart
+++ lib/custom_widgets/fetch_widget.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchAlbum() {
-  return http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Failed to load album")
+  }
 }
 
 class Album {
@@ -10,5 +19,8 @@ class Album {
   final String title;
 
   Album(this.userId, this.id, this.title);
-  
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return new Album(json['userId'], json['id'], json['title']);
+  }
 }
