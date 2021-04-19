c6a94e4a0191eca7e06ff1fc3e5053cc2995111f
danbourdier <dfbourdier@gmail.com>
2021-04-19 17:09:18 -0400

    defined User calss

--- lib/custom_widgets/user_actions.dart
+++ lib/custom_widgets/user_actions.dart
@@ -1,11 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 // our fetch method
-
 
 // Our User Class
 class User {
-  
+  final int userId;
+  final String userName;
+  final String userEmail;
+
+  User({required this.userId, required this.userName, required this.userEmail});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return new User(
+      userEmail: json['email'],
+      userName: json['username'],
+      userId: json['id'],
+    );
+  }
 }
