d7adc8c6f131ff7bb513e2a5c7347ab3fa95193a
danbourdier <dfbourdier@gmail.com>
2021-04-20 11:02:00 -0400

    refactored function call on line 11

--- lib/custom_widgets/user_actions.dart
+++ lib/custom_widgets/user_actions.dart
@@ -3,6 +3,29 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 // our fetch method
+Future<List<User>> fetchUsers() async {
+  final response =
+      await http.get(Uri.https("jsonplaceholder.typicode.com", "users"));
+
+  if (response.statusCode == 200) {
+    return sanitizeUsers(jsonDecode(response.body));
+  } else {
+    throw Exception("Error getting users.");
+  }
+}
+
+// create a function that accepts a List of objects and returns a mapped list of
+// user instances.
+List<User> sanitizeUsers(List<Map<String, dynamic>> users) {
+  late List<User> userList = [];
+  userList.length = users.length;
+
+  users.forEach((user) {
+    userList.add(User.fromJson(jsonDecode(user)));
+  });
+
+  return userList;
+}
 
 // Our User Class
 class User {
